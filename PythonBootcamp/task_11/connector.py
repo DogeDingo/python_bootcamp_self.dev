d9246bcd9d87c4c46e2f217d7c869442b71315f8
DingodogXD <jonnykober@hotmail.de>
2022-09-12 04:28:08 +0200

    Task 11 solutions added

---
+++ PythonBootcamp/task_11/connector.py
@@ -0,0 +1,21 @@
+import psycopg2
+from db_modules import db_module_pro
+
+if __name__ == '__main__':
+    with psycopg2.connect(
+                host=db_module_pro.hostname,
+                dbname=db_module_pro.database,
+                user=db_module_pro.username,
+                password=db_module_pro.pwd,
+                port=db_module_pro.port_id
+                            ) as db_connector:
+
+        with db_connector.cursor() as db_cursor:
+                pass #remove pass and write your PostgreSQL here
+
+
+
+
+    db_connector.close()
+
+
---
+++ PythonBootcamp/task_11/db_modules/db_module_pro.py
@@ -0,0 +1,35 @@
+# my credentials
+
+hostname = 'localhost'
+database = 'playground'
+username = 'postgres'
+pwd = 'jonnyssecret'
+port_id = 5432
+
+# function to create a table in PostgreSQL with user input
+def create_table():
+    name_table = input("Please name your table: ")
+    number_columns = input("How many columns do you need? ")
+    list_columns = []
+    list_records = []
+    number_index = 1
+    while number_index <= int(number_columns):
+        list_columns.append(input(f"Column Number {number_index} name please: "))
+
+        list_records.append(input(f"Please enter datatype (please be very specific - e.g. if you use varchar() please also put a number into the brackets: "))
+        number_index += 1
+
+    lists_records = []
+    for column_name, column_type in zip(list_columns, list_records):
+        string_placeholder = f"{column_name} {column_type}"
+        lists_records.append(string_placeholder)
+
+    records_printed = (" ,".join(lists_records))
+    create_script = f'''CREATE TABLE IF NOT EXISTS {name_table} (
+                          {records_printed})
+                          '''
+    return create_script
+
+
+
+
